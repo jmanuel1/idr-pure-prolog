jmanuel1/idr-pure-prolog

commit 5a2b44b90d87b1910b7719d77a726e49887173bf

Convert source to Idris 2

src/Interp.idr
@@ -9,45 +9,57 @@
 module Interp
 
 import Ast
+import Data.List
 
-%access public export
+%default partial
 
 ----------------------------------------------------------------------
 -- Interpreter
 ----------------------------------------------------------------------
 
+public export
 Unifier : Type
 Unifier = List (Variable, Term)
 
 -- A non-lazy version of maybe
+public export
 maybe' : b -> (a -> b) -> Maybe a -> b
 maybe' n j Nothing  = n
 maybe' n j (Just x) = j x
 
+public export
 subs : Unifier -> Term -> Term
 subs u (Var x)   = maybe' (Var x) id (lookup x u)
-subs u (Comp n ts) = Comp n (map (subs u) ts)
+subs u (Comp n ts) = Comp n $ map (subs u) ts
 
+public export
 compose : Unifier -> Unifier -> Unifier
 compose u1 u2 = (map (\(v, t) => (v, subs u2 t)) u1) ++ u2
 
+public export
 occursIn : Variable -> Term -> Bool
 occursIn v (Var x)     = v == x
 occursIn v (Comp _ ms) = any (occursIn v) ms
 
 mutual
+  public export
+  covering
   unify : Term -> Term -> Maybe Unifier
   unify (Var "_") t                         = pure []
   unify t (Var "_")                         = pure []
   unify (Var x) (Var y) with (x == y)
-     unify (Var x) (Var y) | True           = pure [] 
+     unify (Var x) (Var y) | True           = pure []
+     _ | False = Nothing
   unify (Var x) t with (not (x `occursIn` t))
       unify (Var x) t   | True              = pure [(x, t)]
+      _ | False = Nothing
   unify t v@(Var _)                         = unify v t
   unify (Comp m ms) (Comp n ns) with (m == n)
      unify (Comp m ms) (Comp n ns) | True   = unifyList ms ns
+     _ | False = Nothing
   unify _ _ = Nothing
 
+  public export
   unifyList : Terms -> Terms -> Maybe Unifier
   unifyList (t :: ts) (r :: rs) =
       do u1 <- unify t r
@@ -58,36 +70,45 @@ mutual
   unifyList _ _   = Nothing
 
 mutual
+  public export
   vars : Term -> List Variable
   vars (Var "_") = []
   vars (Var x) = [x]
   vars (Comp _ ts) = varsList ts
 
+  public export
   varsList : Terms -> List Variable
   varsList ts = [ v | t <- ts, v <- vars t]
 
+public export
 variables : Terms -> List Variable
 variables ts = nub $ varsList ts
 
+public export
 freshen : List Variable -> (Term, Terms) -> (Term, Terms)
 freshen bound (tc, tb) = (subs sub tc, map (subs sub) tb)
-    where vars : List Variable
-          vars = variables (tc :: tb)
-          sub = [ (v, Var $ nextVar 0 v) | v <- vars, v `elem` bound]
-	  nextVar : Int -> Variable -> Variable
-          nextVar i v = let v' = "_" ++ show i ++ "_" ++ v in
-                        if v' `elem` bound then nextVar (i+1) v
-                        else v'
+  where
+    vars : List Variable
+    vars = variables (tc :: tb)
+    nextVar : Int -> Variable -> Variable
+    nextVar i v =
+      let v' = "_" ++ show i ++ "_" ++ v in
+        if v' `elem` bound
+          then nextVar (i+1) v
+          else v'
+    sub : Unifier
+    sub = [ (v, Var (nextVar 0 v)) | v <- vars, v `elem` bound]
 
-
+public export
 data Solution : Type where
     MkSolution : List (Variable, Term) -> Solution
 
+public export
 implementation Eq Solution where
     (MkSolution x) == (MkSolution y) = assert_total $ x == y
-    x == y = assert_total $ False
 
 
+public export
 Show Solution where
   show (MkSolution [])            = "True"
   show (MkSolution xs ) = (concat $ intersperse "\n" $ map renderBindings xs)
@@ -95,55 +116,67 @@ Show Solution where
           renderBindings (v, t) = v ++ " = " ++ show t
 
 
+public export
 isPlain : List Char -> Bool
 isPlain (c :: cs) = isLower c && all (\c => c == '_' || isAlphaNum c) cs
 isPlain [] = False
-isPlain _ = False
 
+public export
 data SearchTree : Type where
   Sol : Solution -> SearchTree
   Node : Goal -> List SearchTree -> SearchTree
 
+public export
 Eq SearchTree where
   (Sol x) == (Sol y) = assert_total $ x == y
   (Node x y) == (Node x' y') = assert_total $ (x == x') && (y == y')
   x == y = False
 
+public export
 isReportGoal : Term -> Bool
 isReportGoal (Comp "_report" _) = True
 isReportGoal _                  = False
 
+public export
 getSolution : Term -> Solution
 getSolution (Comp "_report" args) = MkSolution sol
     where nontriv : (String, Term) -> Bool
           nontriv (x, (Var y)) with (x == y)
-	      nontriv (x, (Var y)) | True = False
+            nontriv (x, (Var y)) | True = False
+            nontriv (x, (Var y)) | _ = True
           nontriv _ = True
-	  sol = filter nontriv $ map (\ (Comp "=" [Comp v [], t]) => (v, t)) args
+          sol : ?
+          sol = filter nontriv $ map (\ (Comp "=" [Comp v [], t]) => (v, t)) args
 getSolution _ = MkSolution [("ERROR", Var "_Error")]
 
 -- Uses the List monad for backtracking
+public export
 solve : Program -> Goal -> List SearchTree
 solve prog [r] with (isReportGoal r)
     solve prog [r] | True  =  pure $ Sol $ getSolution r
-    solve prog [r] | False = [] 
+    solve prog [r] | False = []
 solve prog g@(t1 :: ts) = pure $ Node g trees
-    where trees = do c <- prog
-                     let (tc, tsc) = freshen (variables g) c
-                     case unify tc t1 of
-                       Just u => do
-                         let g' = map (subs u) $ tsc ++ ts
-                         solve prog g'
-                       Nothing => []
+    where
+      trees : List SearchTree
+      trees = do
+        c <- prog
+        let (tc, tsc) = freshen (variables g) c
+        case unify tc t1 of
+         Just u => do
+           let g' = map (subs u) $ tsc ++ ts
+           solve prog g'
+         Nothing => []
 
+public export
 makeReportGoal : Terms -> List Term
 makeReportGoal goal = [Comp "_report" reportVars]
     where vars : List Variable
           vars = variables goal
-	  reportVars : Terms
+          reportVars : Terms
           reportVars = map (\ v => Comp "=" [Comp v [], Var v]) vars
 
 -- Use the trick of inserting an extra reporting goal
+public export
 makeReportTree : Program -> Goal -> SearchTree
 makeReportTree prog goal = Node goal $ solve prog (goal ++ makeReportGoal goal)
 
@@ -153,18 +186,22 @@ makeReportTree prog goal = Node goal $ solve prog (goal ++ makeReportGoal goal)
 ----------------------------------------------------------------------
 
 -- Depth first
+public export
 dfs : SearchTree -> List Solution
 dfs (Sol sols) = [sols]
 dfs (Node _ st) = [ s | t <- st, s <- dfs t]
 
 -- Breath first
+public export
 bfs : SearchTree -> List Solution
 bfs t = trav [t]
-    where trav [] = []
+    where trav : List SearchTree -> List Solution
+          trav [] = []
           trav ((Sol x) :: q) = x :: trav q
           trav ((Node _ st)  :: q) = trav (q ++ st)
 
 -- Limited depth first
+public export
 limitedDfs : Int -> SearchTree -> List Solution
 limitedDfs _ (Sol sols)  = [sols]
 limitedDfs 0 _           = []
@@ -178,12 +215,50 @@ limitedDfs n (Node _ st) = [ s | t <- st, s <- limitedDfs (n-1) t]
 -- function to "h" and "b" here in order to supply some more syntatic
 -- sugar.
 
-syntax [h] ":-" [b] = (h, b)
 
+
+-- syntax [h] ":-" [b] = (h, b)
+infix 10 :-
+export
+(:-) : Term -> Terms -> Clause
+head :- body = (head, body)
+
+
+public export
 empty : List Term
 empty = []
 
-syntax [p] "." = (p, empty)
+-- syntax [p] "." = (p, empty)
+-- suffix 10 .
 
-syntax "?-" [q] = nub $ bfs $ makeReportTree program [q]
+export
+data ProgramElement = Term | Clause
 
+export
+interface InterpProgramElement type (0 p : ProgramElement) | type where
+  toClause : type -> Clause
+
+export
+InterpProgramElement Term Term where
+  toClause p = (p, empty)
+
+export
+InterpProgramElement Clause Clause where
+  toClause = id
+
+public export
+data DSLProgram : Type where
+  Nil : DSLProgram
+  (::) : InterpProgramElement type p => type -> DSLProgram -> DSLProgram
+
+export
+unDSL : DSLProgram -> Program
+unDSL [] = []
+unDSL (element :: elements) = toClause element :: unDSL elements
+
+-- syntax "?-" [q] = nub $ bfs $ makeReportTree program [q]
+
+prefix 10 ?-
+export
+(?-) : {auto program : Program} -> Term -> List Solution
+(?-) {program} q = nub $ bfs $ makeReportTree program [q]

src/Ast.idr
@@ -8,49 +8,61 @@
 
 module Ast
 
-%access public export
+import Data.List
 
 ----------------------------------------------------------------------
 -- Abstract Syntax Tree
 ----------------------------------------------------------------------
 
+public export
 Atom : Type
 Atom = String
 
+public export
 Variable : Type
 Variable = String
 
+public export
 data Term : Type where
  Var : Variable -> Term
- Comp : Atom -> List Term -> Term
+ Comp : Atom -> (args: List Term) -> Term
 
-and' : List Bool -> Bool
-and' [] = True
-and' xs = foldr1 p xs
-  where p True True = True
-        p x y = False
+public export
+and' : (l : List Bool) -> Bool
+and' xs = foldr p True xs
+  where
+    p : Bool -> Bool -> Bool
+    p True True = True
+    p x y = False
 
+public export
 implementation Eq Term where
   (Var x) == (Var y) = assert_total $ x == y
-  (Comp x y) == (Comp x' y') = assert_total $ (x == x') && (and' $ Prelude.List.zipWith (==) y y')
+  (Comp x y) == (Comp x' y') = assert_total $ (x == x') && (and' $ zipWith (==) y y')
   x == y = assert_total $ False
 
+public export
 implementation Show Term where
   show (Var s) = s
   show (Comp s []) = s
-  show (Comp s xs) = s ++ "(" ++ (concat $ intersperse "," (map show xs)) ++ ")"
+  show (Comp s xs) = s ++ "(" ++ (concat $ intersperse "," (map (\x => show (assert_smaller xs x)) xs)) ++ ")"
 
+public export
 Terms : Type
 Terms = List Term
 
+public export
 Clause : Type
 Clause = (Term, Terms) -- head and body
 
+public export
 Clauses : Type
 Clauses = List Clause
 
+public export
 Goal : Type
 Goal = List Term
 
+public export
 Program : Type
 Program = Clauses

src/Examples.idr
@@ -8,57 +8,75 @@
 
 import Interp
 import Ast
+import Data.List
 
-%access public export
+%default partial
 
+public export
 p : Term
 p = Comp "p" []
 
+public export
 q : Term
 q = Comp "q" []
 
+public export
 x : Term
 x = Comp "x" []
 
+public export
 z : Term
 z = Comp "z" []
 
+public export
 r : Term -> Term
 r x = Comp "r" [x]
 
+public export
 n : Term -> Term
 n x = Comp "n" [x]
 
+public export
 X : Term
 X = Var "X"
 
+public export
 s : Term -> Term
 s x = Comp "s" [x]
 
+public export
 f : Term -> Term -> Term
 f x y = Comp "f" [x, y]
 
+public export
+%hint
 program : Program
-program = [ p .
-          , q :- [p]
-	  , (n z) .
-	  , (n (s X)) :- [n X]
-	  , (f z z) .
-	  , (f z (s z)) .
-	  , (f X X) .
-	  ]
+program = unDSL
+  [ p
+    , q :- [p]
+    , (n z)
+    , (n (s X)) :- [n X]
+    , (f z z)
+    , (f z (s z))
+    , (f X X)
+  ]
 
+public export
 result : List Solution
 result = nub $ bfs $ makeReportTree program [f z X]
 
 -- Note: I should make this more type-safe.
+public export
 getSolutions : List Solution -> List Term
 getSolutions xs = concat $ map getSolution xs
-  where getSolution (MkSolution x) = map snd x
+  where getSolution : Solution -> List Term
+        getSolution (MkSolution x) = map snd x
 
+public export
 f_val : Term -> List Term
 f_val x = getSolutions $ nub $ bfs $ makeReportTree program [f x X]
 
+public export
 nat : Term -> Bool
 nat t = query /= []
   where query : List Solution
@@ -66,6 +84,7 @@ nat t = query /= []
 
 -- This is a version of f_val that runs on our embedded prolog
 -- system, but only allows values of type Nat.
+public export
 f_val' : (x : Term) -> {auto p : True = nat x} -> List Term
 f_val' x = getSolutions $ ?- f x X
 

src/Tests.idr
@@ -3,19 +3,23 @@ module Main
 import Interp
 import Ast
 import Examples
+import Data.List
+
+%default partial
 
 program2 : Program
-program2 = [ (s z) .
-           , (s (s z)) .
-	   ]
+program2 = unDSL
+  [ (s z)
+  , (s (s z))
+  ]
 
 -- Some tests
 main : IO ()
 main = do
   putStrLn "\n--------------"
-  print $ (nub $ dfs $ makeReportTree [(n z) .] [n X])
+  print $ (nub $ dfs $ makeReportTree (unDSL [(n z)]) [n X])
   putStrLn "\n---------------"
-  print $ (nub $ bfs $ makeReportTree [(n z) .] [n X])
+  print $ (nub $ bfs $ makeReportTree (unDSL [(n z)]) [n X])
   putStrLn "\n---------------"
   -- Why does this one get [True, True]? It matches twice?
   print $ (nub $ bfs $ makeReportTree program2 [s X])

commit e1d8b8c631f44db1be9c787e4c8cd50e03af76de

Unify variables with different names
This fixes a bug I think I introduced in 5a2b44b.

src/Interp.idr
@@ -49,7 +49,7 @@ mutual
   unify t (Var "_")                         = pure []
   unify (Var x) (Var y) with (x == y)
      unify (Var x) (Var y) | True           = pure []
-     _ | False = Nothing
+     _ | False = pure [(x, Var y)]
   unify (Var x) t with (not (x `occursIn` t))
       unify (Var x) t   | True              = pure [(x, t)]
       _ | False = Nothing